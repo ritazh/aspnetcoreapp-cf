c09863604987e691536700c8bc8c76026703ab89
Rita Zhang <rita.z.zhang@gmail.com>
2016-05-24 09:47:47 -0700

    Remove ConfigurationBuilder

--- Program.cs
+++ Program.cs
@@ -7,13 +7,9 @@ namespace aspnetcoreapp
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-            .AddCommandLine(args)
-            .Build();
             
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseConfiguration(config)
                 .UseStartup<Startup>()
                 .Build();
 
