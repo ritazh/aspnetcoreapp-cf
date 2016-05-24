fa7f5ab7b2ac75f10aec619b762bb79a733bb032
Rita Zhang <rita.z.zhang@gmail.com>
2016-05-24 11:07:46 -0700

    Adding ConfigurationBuilder

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace aspnetcoreapp
 {
@@ -7,9 +8,13 @@ namespace aspnetcoreapp
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
             
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseStartup<Startup>()
                 .Build();
 
